0c4ddfab9204b4a4b2c16671fd116f41060c476c
Bhavesh Sharma <sharmabhavesh97@gmail.com>
2022-12-03 03:41:14 +0530

    Completed Teams Model

--- carzone/settings.py
+++ carzone/settings.py
@@ -123,3 +123,6 @@ STATIC_ROOT = os.path.join(BASE_DIR , 'static')
 STATICFILES_DIRS = [
 os.path.join(BASE_DIR,'carzone/static'),
 ]
+
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+MEDIA_URL = '/media/'
--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import Team
 # Register your models here.
+from django.utils.html import format_html
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self,object):
+        return format_html('<img src = "{}" width="40" />'.format(object.photo.url))
+
+    list_display = ('id','thumbnail','first_name','designation','create_date')
+    list_display_links = ('first_name' , 'id')
+    search_fields = ('first_name','last_name','designation')
+admin.site.register(Team,TeamAdmin)
